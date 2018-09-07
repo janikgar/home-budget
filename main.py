fdb5a3cdb69f9a02ed387aa2cb02058042f2d976
janikgar <janikgar@gmail.com>
2018-09-07 12:26:46 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import requests
+import pandas
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+def run():
+  pass
+
+if __name__ == "__main__":
+  run()
